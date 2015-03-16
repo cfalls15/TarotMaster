e4aea154c0ff19592859b5ccbffffa6a78392444
cfalls15 <cfalls15@punahou.edu>
2015-03-16 12:32:46 -1000

    Commit #2, now with game and card classes
    created the game and card classes

---
+++ Card.java
@@ -0,0 +1,13 @@
+public class Card
+  {
+    public String myName;
+    public String myDef;
+    public String myType;
+    
+    public Card(String name, String def, String type)
+    {
+      myName = name;
+      myDef = def;
+      myType = type;
+    }
+}
---
+++ Game.java
@@ -0,0 +1,36 @@
+public class Game
+{
+  //create arraylist
+      Card theFool;
+      theFool = new Card("The Fool",
+                         "new beginnings, discovery, and optimism",
+                         "OPTIMISM");
+      
+     Card theMagician;
+     theMagician = new Card("The Magician",
+                            "determination, journey, and initiative",
+                            "TRANSFORMATION");
+     Card theHighPriestess;
+     theHighPriestess = new Card("The High Priestess",
+                                 "increased knowledge and education",
+                                 "KNOWLEDGE");
+     Card theEmpress;
+     theEmpress = new Card("The Empress",
+                           "fertility and marriage",
+                           "LOVE");
+     Card theEmperor;
+     theEmperor = new Card("The Emperor",
+                           "success, authority, and discipline",
+                           "SUCCESS");
+     Card theHierophant;
+     theHierophant = new Card("The Hierophant",
+                              "spirituality and learning",
+                              "KNOWLEDGE");
+     Card
+           //add cards to arraylist
+       
+       public void play()
+     {
+       
+     }
+    }
