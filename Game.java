0f6bc20158e77e50dc6c2e9faed3d0b27aa7748e
cfalls15 <cfalls15@punahou.edu>
2015-03-16 23:00:21 -1000

    Work Log #4
    got in some base code/started making card deck - 80 minutes
    more tarot research  - 15 minutes

--- Game.java
+++ Game.java
@@ -3,30 +3,47 @@ public class Game
   //create arraylist
       Card theFool;
       theFool = new Card("The Fool",
-                         "new beginnings, discovery, and optimism",
+                         "beginning, spontaneity, faith, embracing folly",
                          "OPTIMISM");
       
      Card theMagician;
      theMagician = new Card("The Magician",
-                            "determination, journey, and initiative",
-                            "TRANSFORMATION");
+                            "taking action, consciousness, concentration, power",
+                            "POWER");
      Card theHighPriestess;
      theHighPriestess = new Card("The High Priestess",
-                                 "increased knowledge and education",
+                                 "serenity, intuition, potential, uncovering a truth/solving a mystery",
                                  "KNOWLEDGE");
      Card theEmpress;
      theEmpress = new Card("The Empress",
-                           "fertility and marriage",
+                           "motherly love, abundance, sensuality, nature",
                            "LOVE");
      Card theEmperor;
      theEmperor = new Card("The Emperor",
-                           "success, authority, and discipline",
-                           "SUCCESS");
+                           "fatherly love, structure, authority, regulation",
+                           "JUSTICE");
      Card theHierophant;
      theHierophant = new Card("The Hierophant",
-                              "spirituality and learning",
+                              "education, belief, conformity, identifying with a group",
                               "KNOWLEDGE");
-     Card
+     Card theLovers;
+     theLovers = new Card("The Lovers",
+                          "dfgdfss",
+                          "LOVE");
+     Card theChariot;
+     theChariot = new Card("The Chariot",
+                           "being victorious, asserting yourself, achieving control",
+                           "SUCCESS");
+     Card theStrength;
+     theStrength = new Card("Strength",
+                            "enduring, being patient, compassion, soft control",
+                            "idkman");
+     Card theHermit;
+     theHermit = new Card("The Hermit",
+                          "introspection, seeking something, giving/receiving guidance, solitude",
+                          "KNOWLEDGE");
+     
+                             
            //add cards to arraylist
        
        public void play()
---
+++ TarotMaster.java
@@ -0,0 +1,12 @@
+public class TarotMaster
+{
+  
+    
+    public static void main( String[] args )
+    {
+      Game g = new Game();
+      g.play();
+        
+      
+  }
+}
