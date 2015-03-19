5691c57e6c8982a43f268acbbfd7be0486033fde
cfalls15 <cfalls15@punahou.edu>
2015-03-18 17:27:01 -1000

    Work Log #5
    Added different classes for different types of cards (which require
    different variables) - 30
    continued to make cards - 45
    Also added the variables myReinforcers and myOpposers, which are the
    cards I want the program to check, as if the card has been dealt with
    any of its reinforcers or opposers, that’ll change the definition.

---
+++ AceCard.java
@@ -0,0 +1,15 @@
+public class AceCard
+  {
+    public String myName;
+    public String myDef;
+    public String myType;
+    public String myAcePair
+    
+    public AceCard(String name, String def, String type, String acePair)
+    {
+      myName = name;
+      myDef = def;
+      myType = type;
+      myAcePair = acePair;
+    }
+}
